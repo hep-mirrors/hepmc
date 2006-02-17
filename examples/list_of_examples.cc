66ca03fd1a074a39de69e498850cab5b73b3583c
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2006-02-17 19:36:31 +0000

    used to create documentation
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/trunk@16 4525493e-7705-40b1-a816-d608a930855b

---
+++ examples/list_of_examples.cc
@@ -0,0 +1,14 @@
+	/** \example example_MyPythia.cc
+	 */
+	/** \example example_MyPythiaWithEventSelection.cc
+	 */
+	/** \example example_EventSelection.cc
+	 */
+	/** \example example_ReadPDGtable.cc
+	 */
+	/** \example example_BuildEventFromScratch.cc
+	 */
+	/** \example example_UsingIterators.cc
+	 */
+
+// This file is used by Doxygen when generating the documentation.
