b18b821ee0a1943a926ed2fcdaf667966a03307e
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2006-06-14 20:47:53 +0000

    adding test for print problem with gcc 4.x
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/trunk@24 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/testPrintBug.cc
@@ -0,0 +1,33 @@
+//
+// Thanks to Bob McElrath and Frank Siegert for this test
+//
+
+#include <fstream>
+
+#include "HepMC/GenEvent.h"
+#include "CLHEP/Vector/LorentzVector.h"
+
+int main(int argc,char* argv[]) 
+{
+  HepMC::GenEvent* p_event;
+  
+  p_event = new HepMC::GenEvent();
+
+  // define an output stream
+  std::ofstream os( "testPrintBug.out" );
+  
+  for(int i=0; i<10; i++) {
+    CLHEP::HepLorentzVector vector(1.0,1.0,1.0,1.0);
+    HepMC::GenVertex* vertex = new HepMC::GenVertex(vector,i);
+    for(int j=0; j<3; j++) {
+      HepMC::GenParticle* particle = new HepMC::GenParticle(vector,1,2);
+      vertex->add_particle_in(particle);
+    }
+    for(int j=0; j<3; j++) {
+      HepMC::GenParticle* particle = new HepMC::GenParticle(vector,1,2);
+      vertex->add_particle_out(particle);
+    }
+    p_event->add_vertex(vertex);
+  }
+  p_event->print(os);
+}
