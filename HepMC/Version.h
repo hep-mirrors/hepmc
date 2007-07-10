19fa4d56e5dd8f2319bfa95137ea48181ac217e5
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2007-07-10 16:44:23 +0000

    add version information
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/trunk@114 4525493e-7705-40b1-a816-d608a930855b

---
+++ HepMC/Version.h
@@ -0,0 +1,39 @@
+#ifndef HEPMC_VERSION_H
+#define HEPMC_VERSION_H
+// ----------------------------------------------------------------------
+//
+// Version.h
+// Author:  Lynn Garren
+//
+//  for now, these are free functions
+//
+// ----------------------------------------------------------------------
+
+#include <string>
+#include <iostream>
+
+namespace HepMC {
+
+void version( );			//!< print HepMC version
+void writeVersion( std::ostream & os );	//!< write HepMC version to os
+std::string versionName( );	//!< return HepMC version
+
+inline std::string versionName( )
+{
+    return "2.01.02";
+}
+
+inline void version( )
+{
+    std::cout << " --------------- HepMC Version " << versionName()
+              << " --------------- " << std::endl;
+}
+
+inline void writeVersion( std::ostream & os )
+{
+    os << "             HepMC Version: " << versionName() << std::endl;
+}
+
+}	// HepMC
+
+#endif // HEPMC_VERSION_H
