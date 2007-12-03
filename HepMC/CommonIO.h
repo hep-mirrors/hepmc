70df8f42fdd3e227464a66caf43eb36608c859bd
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2007-12-03 20:37:06 +0000

    cleanup
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/trunk@191 4525493e-7705-40b1-a816-d608a930855b

--- HepMC/CommonIO.h
+++ HepMC/CommonIO.h
@@ -36,9 +36,6 @@ public:
   // IO_ExtendedAscii is deprecated, but we want to be able to read these files
   std::string IO_ExtendedAscii_Key()     const { return m_io_extendedascii_start; }
   std::string IO_ExtendedAscii_End()     const { return m_io_extendedascii_end; }
-  // recognize the heavily deprecated IO_PDG_ParticleDataTable
-  std::string IO_ParticleDataTable_Key() const { return m_io_pdt_start; }
-  std::string IO_ParticleDataTable_End() const { return m_io_pdt_end; }
   
   // write keys
   void write_IO_GenEvent_Key( std::ostream& );
@@ -47,8 +44,6 @@ public:
   void write_IO_Ascii_End( std::ostream& );
   void write_IO_ExtendedAscii_Key( std::ostream& );
   void write_IO_ExtendedAscii_End( std::ostream& );
-  void write_IO_ParticleDataTable_Key( std::ostream& );
-  void write_IO_ParticleDataTable_End( std::ostream& );
 
   // methods to read input
   int find_file_type( std::istream& );
@@ -63,22 +58,18 @@ public:
   
 protected:
   // methods used by the read_io* methods
-    HeavyIon* read_heavy_ion(std::istream* is);
-    PdfInfo* read_pdf_info(std::istream* is);
-    GenParticle* read_particle(std::istream* is,
-                  TempParticleMap& particle_to_end_vertex );
-    GenVertex* read_vertex(std::istream* is, 
-                  TempParticleMap& particle_to_end_vertex );
+    HeavyIon*    read_heavy_ion( std::istream* );
+    PdfInfo*     read_pdf_info( std::istream* );
+    GenParticle* read_particle( std::istream*, TempParticleMap&  );
+    GenVertex*   read_vertex( std::istream*, TempParticleMap&  );
 
 private:
   std::string m_io_genevent_start;
   std::string m_io_ascii_start;
   std::string m_io_extendedascii_start;
-  std::string m_io_pdt_start;
   std::string m_io_genevent_end;
   std::string m_io_ascii_end;
   std::string m_io_extendedascii_end;
-  std::string m_io_pdt_end;
 
 };
 
@@ -88,11 +79,9 @@ inline CommonIO::CommonIO()
 : m_io_genevent_start("HepMC::IO_GenEvent-START_EVENT_LISTING"),
   m_io_ascii_start("HepMC::IO_Ascii-START_EVENT_LISTING"),
   m_io_extendedascii_start("HepMC::IO_ExtendedAscii-START_EVENT_LISTING"),
-  m_io_pdt_start(),
   m_io_genevent_end("HepMC::IO_GenEvent-END_EVENT_LISTING"),
   m_io_ascii_end("HepMC::IO_Ascii-END_EVENT_LISTING"),
-  m_io_extendedascii_end("HepMC::IO_ExtendedAscii-END_EVENT_LISTING"),
-  m_io_pdt_end()
+  m_io_extendedascii_end("HepMC::IO_ExtendedAscii-END_EVENT_LISTING")
 {}
 
 inline void CommonIO::write_IO_GenEvent_Key( std::ostream& os )
@@ -112,12 +101,6 @@ inline void CommonIO::write_IO_ExtendedAscii_Key( std::ostream& os )
 
 inline void CommonIO::write_IO_ExtendedAscii_End( std::ostream& os )
 { os << m_io_extendedascii_end << "\n"; }
-
-inline void CommonIO::write_IO_ParticleDataTable_Key( std::ostream& os )
-{ os << m_io_pdt_start << "\n"; }
-
-inline void CommonIO::write_IO_ParticleDataTable_End( std::ostream& os )
-{ os << m_io_pdt_end << "\n"; }
 
 }
 
