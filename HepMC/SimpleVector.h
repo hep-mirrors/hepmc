e47d3b684235a5e9168bbdff2034da109a7c36b3
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2007-12-17 23:02:15 +0000

    math methods
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/branches/HEPMC_02_01_branch@211 4525493e-7705-40b1-a816-d608a930855b

--- HepMC/SimpleVector.h
+++ HepMC/SimpleVector.h
@@ -44,71 +44,79 @@ class FourVector {
 public:
 
   /// constructor requiring at least x, y, and z
-  inline FourVector( double xin, double yin, double zin, double tin=0) 
+  FourVector( double xin, double yin, double zin, double tin=0) 
   : m_x(xin), m_y(yin), m_z(zin), m_t(tin) {}
 
   /// constructor requiring only t 
-  inline FourVector(double t)
+  FourVector(double t)
   : m_x(0), m_y(0), m_z(0), m_t(t) {}
 
-  inline FourVector() 
+  FourVector() 
   : m_x(0), m_y(0), m_z(0), m_t(0) {}
 
   /// templated constructor
   /// this is used ONLY if T is not arithmetic
   template <class T >
-  inline FourVector( const T& v,
+  FourVector( const T& v,
          typename detail::disable_if< detail::is_arithmetic<T>::value, void >::type * = 0 )
   : m_x(v.x()), m_y(v.y()), m_z(v.z()), m_t(v.t()) {}
 
   /// copy constructor
-  inline FourVector(const FourVector & v)
+  FourVector(const FourVector & v)
   : m_x(v.x()), m_y(v.y()), m_z(v.z()), m_t(v.t()) {}
 
   void swap( FourVector & other );  //!< swap
 
-  inline double px() const { return m_x; }  //!< return px
-  inline double py() const { return m_y; }  //!< return py
-  inline double pz() const { return m_z; }  //!< return pz
-  inline double e()  const { return m_t; }  //!< return E
+  double px() const { return m_x; }  //!< return px
+  double py() const { return m_y; }  //!< return py
+  double pz() const { return m_z; }  //!< return pz
+  double e()  const { return m_t; }  //!< return E
 
-  inline double x() const { return m_x; }  //!< return x
-  inline double y() const { return m_y; }  //!< return y
-  inline double z() const { return m_z; }  //!< return z
-  inline double t() const { return m_t; }  //!< return t
+  double x() const { return m_x; }  //!< return x
+  double y() const { return m_y; }  //!< return y
+  double z() const { return m_z; }  //!< return z
+  double t() const { return m_t; }  //!< return t
 
-  inline double m2() const;  //!< Invariant mass squared.
-  inline double m() const;   //!< Invariant mass. If m2() is negative then -sqrt(-m2()) is returned.
+  double m2() const;  //!< Invariant mass squared.
+  double m() const;   //!< Invariant mass. If m2() is negative then -sqrt(-m2()) is returned.
 
-  inline double perp2() const;  //!< Transverse component of the spatial vector squared.
-  inline double perp() const;   //!< Transverse component of the spatial vector (R in cylindrical system).
-  inline double mag() const;    //!< Magnitude of the spatial vector
+  double perp2() const;  //!< Transverse component of the spatial vector squared.
+  double perp() const;   //!< Transverse component of the spatial vector (R in cylindrical system).
+  double mag() const;    //!< Magnitude of the spatial vector
 
   // Get spatial vector components in spherical coordinate system.
-  inline double theta() const;  //!< The polar angle.
-  inline double phi() const;  //!< The azimuth angle.
-  inline double rho() const;  //!< spatial vector component magnitude
+  double theta() const;  //!< The polar angle.
+  double phi() const;  //!< The azimuth angle.
+  double rho() const;  //!< spatial vector component magnitude
 
-  inline FourVector & operator = (const FourVector &); //!< make a copy
+  FourVector & operator = (const FourVector &); //!< make a copy
 
-  inline bool operator == (const FourVector &) const; //!< equality
-  inline bool operator != (const FourVector &) const; //!< inequality
+  bool operator == (const FourVector &) const; //!< equality
+  bool operator != (const FourVector &) const; //!< inequality
 
-  inline double pseudoRapidity() const;  //!< Returns the pseudo-rapidity, i.e. -ln(tan(theta/2))
-  inline double eta() const;             //!< Pseudorapidity (of the space part)
+  double pseudoRapidity() const;  //!< Returns the pseudo-rapidity, i.e. -ln(tan(theta/2))
+  double eta() const;             //!< Pseudorapidity (of the space part)
 
   /// set x, y, z, and t
-  inline void set        (double x, double y, double z, double  t);
+  void set        (double x, double y, double z, double  t);
 
-  inline void setX(double x) { m_x=x; }  //!< set x
-  inline void setY(double y) { m_y=y; }  //!< set y
-  inline void setZ(double z) { m_z=z; }  //!< set z
-  inline void setT(double t) { m_t=t; }  //!< set t
+  void setX(double x) { m_x=x; }  //!< set x
+  void setY(double y) { m_y=y; }  //!< set y
+  void setZ(double z) { m_z=z; }  //!< set z
+  void setT(double t) { m_t=t; }  //!< set t
 
-  inline void setPx(double x) { m_x=x; }  //!< set px
-  inline void setPy(double y) { m_y=y; }  //!< set py
-  inline void setPz(double z) { m_z=z; }  //!< set pz
-  inline void setE(double t)  { m_t=t; }  //!< set E
+  void setPx(double x) { m_x=x; }  //!< set px
+  void setPy(double y) { m_y=y; }  //!< set py
+  void setPz(double z) { m_z=z; }  //!< set pz
+  void setE(double t)  { m_t=t; }  //!< set E
+
+  // simple math
+  FourVector   operator +  (const FourVector &) const; //!< addition
+  FourVector & operator += (const FourVector &);       //!< addition
+  FourVector   operator -  (const FourVector &) const; //!< subtraction
+  FourVector & operator -= (const FourVector &);       //!< subtraction
+  FourVector   operator -  () const;                   //!< unary minus
+  FourVector & operator *= (double);                   //!< scale
 
 private:
 
@@ -134,50 +142,58 @@ class ThreeVector {
 public:
 
   /// construct using x, y, and z (only x is required)
-  inline ThreeVector( double xin, double yin =0, double zin =0 ) 
+  ThreeVector( double xin, double yin =0, double zin =0 ) 
   : m_x(xin), m_y(yin), m_z(zin) {}
 
-  inline ThreeVector( ) 
+  ThreeVector( ) 
   : m_x(0), m_y(0), m_z(0) {}
   
   /// templated constructor
   /// this is used ONLY if T is not arithmetic
   template <class T >
-  inline ThreeVector( const T& v,
+  ThreeVector( const T& v,
          typename detail::disable_if< detail::is_arithmetic<T>::value, void >::type * = 0 )
   : m_x(v.x()), m_y(v.y()), m_z(v.z()) {}
 
   /// copy constructor
-  inline ThreeVector(const ThreeVector & v)
+  ThreeVector(const ThreeVector & v)
   : m_x(v.x()), m_y(v.y()), m_z(v.z()) {}
 
   void swap( ThreeVector & other );  //!< swap
 
-  inline double x() const { return m_x; }  //!< return x
-  inline double y() const { return m_y; }  //!< return y
-  inline double z() const { return m_z; }  //!< return z
+  double x() const { return m_x; }  //!< return x
+  double y() const { return m_y; }  //!< return y
+  double z() const { return m_z; }  //!< return z
 
-  inline void setX(double x) { m_x=x; }  //!< set x
-  inline void setY(double y) { m_y=y; }  //!< set y
-  inline void setZ(double z) { m_z=z; }  //!< set z
-  inline void set( double x, double y, double z);   //!< set x, y, and z
+  void setX(double x) { m_x=x; }  //!< set x
+  void setY(double y) { m_y=y; }  //!< set y
+  void setZ(double z) { m_z=z; }  //!< set z
+  void set( double x, double y, double z);   //!< set x, y, and z
 
-  inline double phi()   const;  //!< The azimuth angle.
-  inline double theta() const;  //!< The polar angle.
-  inline double r()     const;  //!< The magnitude
+  double phi()   const;  //!< The azimuth angle.
+  double theta() const;  //!< The polar angle.
+  double r()     const;  //!< The magnitude
 
-  inline double mag() const;  //!< The magnitude (r in spherical coordinate system).
+  double mag() const;  //!< The magnitude (r in spherical coordinate system).
 
-  inline void setPhi(double);  //!< Set phi keeping mag and theta constant (BaBar).
-  inline void setTheta(double);  //!< Set theta keeping mag and phi constant (BaBar).
+  void setPhi(double);  //!< Set phi keeping mag and theta constant (BaBar).
+  void setTheta(double);  //!< Set theta keeping mag and phi constant (BaBar).
 
-  inline double perp2() const;  //!< The transverse component squared (rho^2 in cylindrical coordinate system).
-  inline double perp() const;  //!< The transverse component (rho in cylindrical coordinate system).
+  double perp2() const;  //!< The transverse component squared (rho^2 in cylindrical coordinate system).
+  double perp() const;  //!< The transverse component (rho in cylindrical coordinate system).
 
-  inline ThreeVector & operator = (const ThreeVector &); //!< make a copy
+  ThreeVector & operator = (const ThreeVector &); //!< make a copy
 
-  inline bool operator == (const ThreeVector &) const; //!< equality
-  inline bool operator != (const ThreeVector &) const; //!< inequality
+  bool operator == (const ThreeVector &) const; //!< equality
+  bool operator != (const ThreeVector &) const; //!< inequality
+
+  // simple math
+  ThreeVector   operator +  (const ThreeVector &) const; //!< addition
+  ThreeVector & operator += (const ThreeVector &);       //!< addition
+  ThreeVector   operator -  (const ThreeVector &) const; //!< subtraction
+  ThreeVector & operator -= (const ThreeVector &);       //!< subtraction
+  ThreeVector   operator -  () const;                    //!< unary minus
+  ThreeVector & operator *= (double);                    //!< scale
 
 private:
 
--- test/testSimpleVector.cc
+++ test/testSimpleVector.cc
@@ -5,6 +5,9 @@
 
 #include "HepMC/SimpleVector.h"
 
+int threeVectorMath();
+int fourVectorMath();
+
 int main() 
 {
   // ThreeVector
@@ -42,34 +45,36 @@ int main()
   if( v3 != v3copy ) {
      std::cout << "vectors v3 and v3copy are different" << std::endl;
   }
+
+  numbad += threeVectorMath();
  
   // FourVector
-  HepMC::FourVector vector;
-  HepMC::FourVector v(1.1,2.2,3.3,4.4);
+  HepMC::FourVector vector, v4scale1;
+  HepMC::FourVector v4(1.1,2.2,3.3,4.4);
   HepMC::FourVector vt(1.34);
   
-  HepMC::FourVector vectorcopy( v );
-  vector = v;
+  HepMC::FourVector vectorcopy( v4 );
+  vector = v4;
   
-  double px = v.px();
-  double py = v.py();
-  double pz = v.pz();
-  double e  = v.e();
+  double px = v4.px();
+  double py = v4.py();
+  double pz = v4.pz();
+  double e  = v4.e();
    x = vectorcopy.x();
    y = vectorcopy.y();
    z = vectorcopy.z();
   double t = vectorcopy.t();
   
-   p2 = v.perp2();
-   pt = v.perp();
-   l = v.mag();
-   th = v.theta();
-   ph = v.phi();
-   r = v.rho();
-  double masssq1 = v.m2();
-  double mass1 = v.m();
-  double pr1 = v.pseudoRapidity();
-  double eta1 = v.eta();
+   p2 = v4.perp2();
+   pt = v4.perp();
+   l = v4.mag();
+   th = v4.theta();
+   ph = v4.phi();
+   r = v4.rho();
+  double masssq1 = v4.m2();
+  double mass1 = v4.m();
+  double pr1 = v4.pseudoRapidity();
+  double eta1 = v4.eta();
   double masssq2 = vector.m2();
   double mass2 = vector.m();
   double pr2 = vector.pseudoRapidity();
@@ -117,14 +122,137 @@ int main()
      std::cout << "different eta values: " << eta1 << " " << eta2 << std::endl;
      ++numbad;
   }
-  if( v == vector ) {
+  if( v4 == vector ) {
   } else {
      std::cout << "vectors v and vector are different" << std::endl;
      ++numbad;
   }
-  if( v != vectorcopy ) {
+  if( v4 != vectorcopy ) {
      std::cout << "vectors v and vectorcopy are different" << std::endl;
      ++numbad;
   }
+
+  numbad += fourVectorMath();
+
   return numbad;
+}
+
+int threeVectorMath()
+{
+  HepMC::ThreeVector v3(1.1,2.2,3.3);
+   
+  double wt = 1.2528;
+  int n3bad = 0;
+  
+  // scale
+  HepMC::ThreeVector v3scale1 = v3;
+  v3scale1 *= wt;
+  HepMC::ThreeVector v3scale2(wt*v3.x(),wt*v3.y(),wt*v3.z());
+  if( v3scale1 != v3scale2 ) {
+     std::cout << "vectors v3scale1 and v3scale2 are different" << std::endl;
+     ++n3bad;
+  }
+
+  // add
+  HepMC::ThreeVector v3add1 = v3;
+  v3add1 += v3scale1;
+  HepMC::ThreeVector v3add2 = v3 + v3scale1;
+  HepMC::ThreeVector v3add3(v3scale1.x()+v3.x(),
+			   v3scale1.y()+v3.y(),
+			   v3scale1.z()+v3.z());
+  if( v3add1 != v3add2 ) {
+     std::cout << "vectors v3add1 and v3add2 are different" << std::endl;
+     ++n3bad;
+  }
+  if( v3add3 != v3add2 ) {
+     std::cout << "vectors v3add3 and v3add2 are different" << std::endl;
+     ++n3bad;
+  }
+
+  // subtract
+  HepMC::ThreeVector v3sub1 = v3;
+  v3sub1 -= v3scale1;
+  HepMC::ThreeVector v3sub2 = v3 - v3scale1;
+  HepMC::ThreeVector v3sub3(v3.x()-v3scale1.x(),
+			   v3.y()-v3scale1.y(),
+			   v3.z()-v3scale1.z());
+  if( v3sub1 != v3sub2 ) {
+     std::cout << "vectors v3sub1 and v3sub2 are different" << std::endl;
+     ++n3bad;
+  }
+  if( v3sub3 != v3sub2 ) {
+     std::cout << "vectors v3sub3 and v3sub2 are different" << std::endl;
+     ++n3bad;
+  }
+
+  // minus
+  HepMC::ThreeVector v3minus1 = -v3;
+  HepMC::ThreeVector v3minus2(-v3.x(),-v3.y(),-v3.z());
+  if( v3minus1 != v3minus2 ) {
+     std::cout << "vectors v3minus1 and v3minus2 are different" << std::endl;
+     ++n3bad;
+  }
+
+  return n3bad;
+}
+
+int fourVectorMath()
+{
+  HepMC::FourVector v4(1.1,2.2,3.3,4.4);
+   
+  double wt = 1.2528;
+  int n4bad = 0;
+  
+  // scale
+  HepMC::FourVector v4scale1 = v4;
+  v4scale1 *= wt;
+  HepMC::FourVector v4scale2(wt*v4.x(),wt*v4.y(),wt*v4.z(),wt*v4.t());
+  if( v4scale1 != v4scale2 ) {
+     std::cout << "vectors v4scale1 and v4scale2 are different" << std::endl;
+     ++n4bad;
+  }
+
+  // add
+  HepMC::FourVector v4add1 = v4;
+  v4add1 += v4scale1;
+  HepMC::FourVector v4add2 = v4 + v4scale1;
+  HepMC::FourVector v4add3(v4scale1.x()+v4.x(),
+			   v4scale1.y()+v4.y(),
+			   v4scale1.z()+v4.z(),
+			   v4scale1.t()+v4.t());
+  if( v4add1 != v4add2 ) {
+     std::cout << "vectors v4add1 and v4add2 are different" << std::endl;
+     ++n4bad;
+  }
+  if( v4add3 != v4add2 ) {
+     std::cout << "vectors v4add3 and v4add2 are different" << std::endl;
+     ++n4bad;
+  }
+
+  // subtract
+  HepMC::FourVector v4sub1 = v4;
+  v4sub1 -= v4scale1;
+  HepMC::FourVector v4sub2 = v4 - v4scale1;
+  HepMC::FourVector v4sub3(v4.x()-v4scale1.x(),
+			   v4.y()-v4scale1.y(),
+			   v4.z()-v4scale1.z(),
+			   v4.t()-v4scale1.t());
+  if( v4sub1 != v4sub2 ) {
+     std::cout << "vectors v4sub1 and v4sub2 are different" << std::endl;
+     ++n4bad;
+  }
+  if( v4sub3 != v4sub2 ) {
+     std::cout << "vectors v4sub3 and v4sub2 are different" << std::endl;
+     ++n4bad;
+  }
+
+  // minus
+  HepMC::FourVector v4minus1 = -v4;
+  HepMC::FourVector v4minus2(-v4.x(),-v4.y(),-v4.z(),-v4.t());
+  if( v4minus1 != v4minus2 ) {
+     std::cout << "vectors v4minus1 and v4minus2 are different" << std::endl;
+     ++n4bad;
+  }
+
+  return n4bad;
 }
