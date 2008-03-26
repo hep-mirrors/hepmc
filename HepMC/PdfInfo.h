b60f77c30483a938b550d4e29d51e15fbb45b989
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2008-03-26 22:39:27 +0000

    adding pdf id numbers
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/trunk@275 4525493e-7705-40b1-a816-d608a930855b

--- HepMC/PdfInfo.h
+++ HepMC/PdfInfo.h
@@ -10,9 +10,12 @@
 //
 //      int id1; 	// flavour code of first parton
 //      int id2; 	// flavour code of second parton 
+//      int pdf_id1; 	// LHAPDF set id of first parton (zero by default)
+//      int pdf_id2; 	// LHAPDF set id of second parton  (zero by default)
 //      double x1;  	// fraction of beam momentum carried by first parton ("beam side") 
 //      double x2;  	// fraction of beam momentum carried by second parton ("target side") 
 //      double scalePDF;  //  Q-scale used in evaluation of PDF's   (in GeV) 
+//      pdf1 and pdf2 should store x*f(x)
 //      double pdf1;  	// PDF (id1, x1, Q) 
 //      double pdf2;  	// PDF (id2, x2, Q)   
 //
@@ -36,6 +39,8 @@ public:
   PdfInfo()
     : m_id1(0), 
       m_id2(0),
+      m_pdf_id1(0),
+      m_pdf_id2(0),
       m_x1(0),
       m_x2(0),
       m_scalePDF(0),
@@ -44,7 +49,9 @@ public:
     {}
 
   /// all values must be provided
-  PdfInfo( int i1, int i2, double x1, double x2, double q, double p1, double p2 );
+  PdfInfo( int i1, int i2, double x1, double x2,
+           double q, double p1, double p2,
+	   int pdf_id1 = 0, int pdf_id2 = 0 );
 
   ~PdfInfo() {}
     
@@ -64,15 +71,19 @@ public:
     int   id1()                   const { return m_id1; }
     /// flavour code of second parton 
     int   id2()                   const { return m_id2; }
+    /// LHAPDF set id of first parton
+    int   pdf_id1()                   const { return m_pdf_id1; }
+    /// LHAPDF set id of second parton 
+    int   pdf_id2()                   const { return m_pdf_id2; }
     /// fraction of beam momentum carried by first parton ("beam side") 
     double x1()                   const { return m_x1; } 
     /// fraction of beam momentum carried by second parton ("target side") 
     double x2()                   const { return m_x2; }
     ///  Q-scale used in evaluation of PDF's   (in GeV) 
     double scalePDF()             const { return m_scalePDF;  }
-    /// PDF (id1, x1, Q) 
+    /// PDF (id1, x1, Q) - x*f(x)
     double pdf1()                 const { return m_pdf1; }
-    /// PDF (id2, x2, Q)   
+    /// PDF (id2, x2, Q) - x*f(x)
     double pdf2()                 const { return m_pdf2; }
 
   // ---  mutators:
@@ -80,20 +91,26 @@ public:
     void   set_id1(const int &i)              { m_id1=i; }
     /// set flavour code of second parton 
     void   set_id2(const int &i)              { m_id2=i; }
+    /// set LHAPDF set id of first parton
+    void   set_pdf_id1(const int &i)              { m_pdf_id1=i; }
+    /// set LHAPDF set id of second parton 
+    void   set_pdf_id2(const int &i)              { m_pdf_id2=i; }
     /// set fraction of beam momentum carried by first parton ("beam side") 
     void   set_x1(const double &f)            { m_x1=f; }
     /// set fraction of beam momentum carried by second parton ("target side") 
     void   set_x2(const double &f)            { m_x2=f; }
     /// set Q-scale used in evaluation of PDF's   (in GeV) 
     void   set_scalePDF(const double &f)      { m_scalePDF=f;  }
-    /// set PDF (id1, x1, Q) 
+    /// set x*f(x) of first parton
     void   set_pdf1(const double &f)          { m_pdf1=f; }
-    /// set PDF (id2, x2, Q)   
+    /// set x*f(x) of second parton
     void   set_pdf2(const double &f)          { m_pdf2=f; }
 
 private: // data members
     int   m_id1; 
     int   m_id2; 
+    int   m_pdf_id1; 
+    int   m_pdf_id2; 
     double m_x1;
     double m_x2;
     double m_scalePDF; 
@@ -103,9 +120,13 @@ private: // data members
 };
 
 // inline operators
-inline PdfInfo::PdfInfo( int i1, int i2, double x1, double x2, double q, double p1, double p2 )
+inline PdfInfo::PdfInfo( int i1, int i2, double x1, double x2,
+        		 double q, double p1, double p2,
+			 int pdf_id1, int pdf_id2 )
     : m_id1(i1), 
       m_id2(i2),
+      m_pdf_id1(pdf_id1),
+      m_pdf_id2(pdf_id2),
       m_x1(x1),
       m_x2(x2),
       m_scalePDF(q),
@@ -116,6 +137,8 @@ inline PdfInfo::PdfInfo( int i1, int i2, double x1, double x2, double q, double 
 inline PdfInfo::PdfInfo( PdfInfo const & orig )
     : m_id1(orig.m_id1), 
       m_id2(orig.m_id2),
+      m_pdf_id1(orig.m_pdf_id1), 
+      m_pdf_id2(orig.m_pdf_id2),
       m_x1(orig.m_x1),
       m_x2(orig.m_x2),
       m_scalePDF(orig.m_scalePDF),
@@ -134,6 +157,8 @@ inline void PdfInfo::swap( PdfInfo & other )
 {
   std::swap(m_id1, other.m_id1); 
   std::swap(m_id2, other.m_id2);
+  std::swap(m_pdf_id1, other.m_pdf_id1); 
+  std::swap(m_pdf_id2, other.m_pdf_id2);
   std::swap(m_x1, other.m_x1);
   std::swap(m_x2, other.m_x2);
   std::swap(m_scalePDF, other.m_scalePDF);
@@ -146,6 +171,8 @@ inline bool    PdfInfo::operator==( const PdfInfo& a ) const
     /// equality requires that each member match
     return (    a.id1() == this->id1() 
              && a.id2() == this->id2() 
+             && a.pdf_id1() == this->pdf_id1() 
+             && a.pdf_id2() == this->pdf_id2() 
 	     && a.x1() == this->x1() 
 	     && a.x2() == this->x2()
 	     && a.scalePDF() == this->scalePDF() 
