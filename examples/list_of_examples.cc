385600e439d438f74f05f8b58e536a90bd5d93e8
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2008-04-25 19:24:54 +0000

    add tests to example list
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/trunk@309 4525493e-7705-40b1-a816-d608a930855b

--- examples/list_of_examples.cc
+++ examples/list_of_examples.cc
@@ -1,20 +1,44 @@
-	/** \example example_MyPythia.cc
-	 */
-	/** \example example_MyPythiaOnlyToHepMC.cc
-	 */
-	/** \example example_MyHerwig.cc
-	 */
-	/** \example example_EventSelection.cc
-	 */
-	/** \example example_BuildEventFromScratch.cc
-	 */
-	/** \example example_UsingIterators.cc
-	 */
-	/** \example testHerwigCopies.cc
-	 */
-	/** \example testPythiaCopies.cc
-	 */
-	/** \example VectorConversion.h
-	 */
+	/// example to generate events and write output
+	/// example to generate events and perform simple event selection
+	/// example to read the file written by pythia_out
+	/// example to generate events, write them, and read them back
+	/// \example example_MyPythia.cc
+	 
+	/// Example of generating events with Pythia using HepMC/PythiaWrapper.h 
+	/// Events are read into the HepMC event record from the FORTRAN HEPEVT 
+	/// common block using the IO_HEPEVT strategy -- nothing is done with them.
+	/// This program is just used to find the total time required to transfer
+	/// from HEPEVT into the HepMC event record.
+	/// \example example_MyPythiaOnlyToHepMC.cc
+	 
+	/// \example example_MyHerwig.cc
+	 
+	/// Example of applying an event selection to the events written to file
+	/// using example_MyPythia.cxx
+	/// Events containing a photon of pT > 25 GeV pass the selection and are
+	/// written to "example_EventSelection.dat"
+	/// \example example_EventSelection.cc
+	 
+	/// Example of building an event and a particle data table from scratch
+	/// This is meant to be of use for persons implementing HepMC inside a MC 
+	/// event generator
+	/// \example example_BuildEventFromScratch.cc
+
+	/// This example shows low to use the particle and vertex iterators
+	/// \example example_UsingIterators.cc
+	 
+	 
+        /// Multiple events in memory at the same time
+	/// \example testHerwigCopies.cc
+	 
+	/// Multiple events in memory at the same time
+	/// \example testPythiaCopies.cc
+	 
+	/// This example converts from ThreeVector and FourVector to 
+	/// CLHEP::Hep3Vector and CLHEP::HepLorentzVector
+	/// Similar (or perhaps templated) conversion methods could be added to
+	/// any vector class.
+	/// \example VectorConversion.h
+	 
 
 // This file is used by Doxygen when generating the documentation.
---
+++ test/list_of_examples.cc
@@ -0,0 +1,46 @@
+/// The HepMC tests can also serve as useful examples
+
+	/// This program checks the deprecated IO_Ascii and IO_ExtendedAscii
+	/// Apply an event selection to the events in testAscii.input
+	/// Events containing a photon of pT > 25 GeV pass the selection and are
+	/// written to "testAscii.dat"
+	/// Also read and write events using IO_ExtendedAscii
+	/// \example testDeprecated.cc.in
+
+	/// based on example_EventSelection
+	/// Apply an event selection to the events in testHepMC.input
+	/// Events containing a photon of pT > 25 GeV pass the selection and are
+	/// written to "testHepMC.out"
+	/// Add arbitrary PDF information to the good events
+	/// Also write events using IO_AsciiParticles 
+	/// \example testHepMC.cc.in
+
+	/// Use Matt's example_EventSelection along with example_UsingIterators 
+	/// to check HepMC iteration.
+	/// Apply an event selection to the events in testHepMC.input
+	/// Events containing a photon of pT > 25 GeV pass the selection.
+	/// Use iterators on these events.
+	/// \example testHepMCIteration.cc.in
+
+	/// Read events from testIOGenEvent.input
+	/// Select events containing a photon of pT > 25 GeV 
+	/// Add arbitrary PDF information to one of the good events
+	/// Write the selected events and read them back in using an istream
+	/// \example testMass.cc.in
+
+	/// Multiple events in memory at the same time
+	/// run with valgrind or some other leak checker
+	/// \example testMultipleCopies.cc.in
+
+        /// Thanks to Bob McElrath and Frank Siegert for this test
+	/// \example testPrintBug.cc
+
+        /// Exercise all the vector methods
+	/// \example testSimpleVector.cc
+
+	/// Test MomentumUnits and PositionUnits
+	/// Make sure set and change methods work as expected.
+	/// \example testUnits.cc
+
+
+// This file is used by Doxygen when generating the documentation.
