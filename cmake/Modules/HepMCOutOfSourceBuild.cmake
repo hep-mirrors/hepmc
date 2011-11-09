e140c9e5cd7957a35e24720926d384080ba21112
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2011-11-09 20:33:54 +0000

    adding cmake build option
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/trunk@483 4525493e-7705-40b1-a816-d608a930855b

---
+++ cmake/Modules/HepMCOutOfSourceBuild.cmake
@@ -0,0 +1,20 @@
+# Throw a fatal error if cmake is invoked from within the source code directory tree
+# hepmc_ensure_out_of_source_build()
+#
+
+macro (hepmc_ensure_out_of_source_build)
+
+  string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" in_source)
+  string( REGEX MATCH "${CMAKE_SOURCE_DIR}/" in_source_subdir "${CMAKE_BINARY_DIR}")
+  if (in_source OR in_source_subdir)
+  message(FATAL_ERROR "
+ERROR: In source builds of this project are not allowed.
+A separate build directory is required.
+Please create one and run cmake from the build directory.
+Also note that cmake has just added files to your source code directory.
+We suggest getting a new copy of the source code.
+Otherwise, delete `CMakeCache.txt' and the directory `CMakeFiles'.
+  ")
+  endif ()
+
+endmacro (hepmc_ensure_out_of_source_build)
---
+++ cmake/Modules/HepMCUseCpack.cmake
@@ -0,0 +1,45 @@
+# define the environment for cpack
+#
+include(HepMCFindCompiler)
+
+# note that hepmc_parse_version is used to define VERSION_MAJOR, etc.
+set( CPACK_PACKAGE_VERSION_MAJOR ${VERSION_MAJOR} )
+set( CPACK_PACKAGE_VERSION_MINOR ${VERSION_MINOR} )
+set( CPACK_PACKAGE_VERSION_PATCH ${VERSION_PATCH} )
+
+set( CPACK_INCLUDE_TOPLEVEL_DIRECTORY 0 )
+set( CPACK_GENERATOR TGZ )
+
+hepmc_find_compiler()
+
+if ( ${SLTYPE} MATCHES "noarch" )
+  set( PACKAGE_BASENAME ${SLTYPE} )
+else ()
+  if ( NOT CPack_COMPILER_STRING )
+    set( PACKAGE_BASENAME ${SLTYPE}-${CMAKE_SYSTEM_PROCESSOR} )
+  else ()
+    set( PACKAGE_BASENAME ${SLTYPE}-${CMAKE_SYSTEM_PROCESSOR}${CPack_COMPILER_STRING} )
+  endif ()
+endif ()
+if ( NOT qualifier )
+  set( CPACK_SYSTEM_NAME ${PACKAGE_BASENAME} )
+else ()
+  set( CPACK_SYSTEM_NAME ${PACKAGE_BASENAME}-${qualifier} )
+endif ()
+# check for extra qualifiers
+if( NOT  CMAKE_BUILD_TYPE )
+   SET( CMAKE_BUILD_TYPE_TOLOWER default )
+else()
+   STRING(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_TOLOWER)
+   if( ${CMAKE_BUILD_TYPE_TOLOWER} MATCHES "debug")
+      set(CPACK_SYSTEM_NAME ${CPACK_SYSTEM_NAME}-debug )
+   elseif( ${CMAKE_BUILD_TYPE_TOLOWER} MATCHES "release")
+      set(CPACK_SYSTEM_NAME ${CPACK_SYSTEM_NAME}-opt )
+   elseif( ${CMAKE_BUILD_TYPE_TOLOWER} MATCHES "minsizerel")
+      set(CPACK_SYSTEM_NAME ${CPACK_SYSTEM_NAME}-prof )
+   endif()   
+endif()
+
+message(STATUS "CPACK_SYSTEM_NAME = ${CPACK_SYSTEM_NAME}" )
+
+include(CPack)
