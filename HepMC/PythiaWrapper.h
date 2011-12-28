a7d0c1193430f132776b2662e920182acbf4d66f
garren <garren@4525493e-7705-40b1-a816-d608a930855b>
2011-12-28 17:35:05 +0000

    merging changes from head
    git-svn-id: svn+ssh://svn.cern.ch/reps/hepmc/branches/HEPMC_02_07_proposal@495 4525493e-7705-40b1-a816-d608a930855b

--- HepMC/PythiaWrapper.h
+++ HepMC/PythiaWrapper.h
@@ -18,12 +18,28 @@
 
 #endif // Platform
 
+#include <cmath>
+
 #include "HepMC/GenCrossSection.h"
 
 namespace HepMC {
 
 /// calculate the Pythia cross section and statistical error
-GenCrossSection getPythiaCrossSection();
+inline GenCrossSection getPythiaCrossSection() {
+
+  GenCrossSection xsec;
+  // xsec(0,2) contains the sum of differential cross sections in mb
+  // ngen(0,2) contains the combined number of generated events
+  // convert to pb (HepMC convention)
+  double xsecval = pyint5.xsec[2][0] * 1.0e9;
+  // statistical error
+  double xsecerr = xsecval / std::sqrt( (double)pyint5.ngen[2][0] );
+  // set and return cross section information
+  xsec.set_cross_section(xsecval, xsecerr);
+  return xsec;
+}
+
+
 
 } // HepMC
 
