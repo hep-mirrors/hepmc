a703593c503a3001da3000c6cfb92ee03a2f22ce
Andy Buckley <andy.buckley@cern.ch>
2020-06-03 09:58:26 +0100

    Add a HEPMC_HAS_ORDERED_WEIGHTS #define.

--- HepMC/HepMCDefs.h
+++ HepMC/HepMCDefs.h
@@ -10,47 +10,47 @@
 //
 // ----------------------------------------------------------------------
 
-// the HeavyIon class is available in HepMC  
+// the HeavyIon class is available in HepMC
 #ifndef HEPMC_HAS_HEAVY_ION
 #define HEPMC_HAS_HEAVY_ION
 #endif
 
-// the IO_Ascii class is NOT available in HepMC   
+// the IO_Ascii class is NOT available in HepMC
 #ifndef HEPMC_IO_ASCII_REMOVED
 #define HEPMC_IO_ASCII_REMOVED
 #endif
 
-// the ParticleData class is NOT available in HepMC   
+// the ParticleData class is NOT available in HepMC
 #ifndef HEPMC_PARTICLE_DATA_REMOVED
 #define HEPMC_PARTICLE_DATA_REMOVED
 #endif
 
-// the IO_GenEvent class is available in HepMC   
+// the IO_GenEvent class is available in HepMC
 #ifndef HEPMC_HAS_IO_GENEVENT
 #define HEPMC_HAS_IO_GENEVENT
 #endif
 
-// the PdfInfo class is available in HepMC  
+// the PdfInfo class is available in HepMC
 #ifndef HEPMC_HAS_PDF_INFO
 #define HEPMC_HAS_PDF_INFO
 #endif
 
-// HepMC uses SimpleVector (FourVector) to store momentum and position  
+// HepMC uses SimpleVector (FourVector) to store momentum and position
 #ifndef HEPMC_HAS_SIMPLE_VECTOR
 #define HEPMC_HAS_SIMPLE_VECTOR
 #endif
 
-// units are defined in HepMC  
+// units are defined in HepMC
 #ifndef HEPMC_HAS_UNITS
 #define HEPMC_HAS_UNITS
 #endif
 
-// the GenCrossSection class is available in HepMC  
+// the GenCrossSection class is available in HepMC
 #ifndef HEPMC_HAS_CROSS_SECTION
 #define HEPMC_HAS_CROSS_SECTION
 #endif
 
-// the iterator range classes are available in HepMC  
+// the iterator range classes are available in HepMC
 #ifndef HEPMC_HAS_ITERATOR_RANGES
 #define HEPMC_HAS_ITERATOR_RANGES
 #endif
@@ -60,9 +60,14 @@
 #define HEPMC_HAS_NAMED_WEIGHTS
 #endif
 
-// define the version of HepMC. 
+// the HepMC::WeightContainer class preserves weight order
+#ifndef HEPMC_HAS_ORDERED_WEIGHTS
+#define HEPMC_HAS_ORDERED_WEIGHTS
+#endif
+
+// define the version of HepMC.
 #ifndef HEPMC_VERSION
-#define HEPMC_VERSION "2.06.10"
+#define HEPMC_VERSION "2.06.11"
 #endif
 
 #endif  // HEPMC_DEFS_H
